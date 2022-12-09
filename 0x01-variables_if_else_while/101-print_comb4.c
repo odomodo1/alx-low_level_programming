d3059e8ee81dd1212a232de3acf5a0914a70c991
odomodo1 <odomodo19@gmail.com>
2022-12-09 14:10:23 -0800

    harder the more

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,24 +2,24 @@
 
 /**
  * main - Prints all possible combination of three different digit,
- * 	in ascending order, separated by a comma followed by a space.
+ * in ascending order, separated by a comma followed by a space.
  * Return: Always 0
  */
 int main(void)
 {
 	int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit1 < 5; digit1++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 6; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			for (digit3 = digit2 + 1; digit3 < 7; digit3++)
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				putchar((digit1 % 7) + '0');
-				putchar((digit2 % 7) + '0');
-				putchar((digit3 % 7) + '0');
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
 
-				if (digit1 == 4 && digit2 == 5 && digit3 == 6)
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
 
 				putchar(',');
