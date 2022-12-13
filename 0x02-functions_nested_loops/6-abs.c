c258eadb3a08497bf752df6194439c0304adda57
odomodo1 <odomodo19@gmail.com>
2022-12-13 09:42:32 -0800

    no such thing as absolute value in this world

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolut value of integer
+ * @f: the integer to be computed
+ * Return: the absolute value
+ */
+int _abs(int f)
+{
+	if (f >= 0)
+		return (f);
+	else
+		return (-f);
+}
