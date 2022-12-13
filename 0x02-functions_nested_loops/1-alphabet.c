246d264df736874e49a5da2b89976290c487b16f
odomodo1 <odomodo19@gmail.com>
2022-12-13 05:53:06 -0800

    write alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the letters in lowercase
+ */
+void print_alphabet(void)
+{
+	char pa;
+
+	for (pa = 'a'; pa <= 'z'; pa++)
+		_putchar(pa);
+
+	_putchar('\n');
+}
