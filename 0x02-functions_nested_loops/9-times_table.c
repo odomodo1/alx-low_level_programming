cbb832ffeb752668e6f40de3daab441b4b580bc9
odomodo1 <odomodo19@gmail.com>
2022-12-13 23:23:38 -0800

    tables of number

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 times table
+ */
+void times_table(void)
+{
+	int num, multiply, produce;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (multiply = 1; multiply <= 9; multiply++)
+		{
+			_putchar(';');
+			_putchar(' ');
+
+			produce = num * multiply;
+
+			if (produce <= 9)
+				_putchar(' ');
+			else
+				_putchar((produce / 10) + '0');
+
+			_putchar((produce % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
