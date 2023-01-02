f9c6b324ad47a48753d94be886b7df0b88a14ed4
odomodo1 <odomodo19@gmail.com>
2023-01-02 10:42:04 -0800

    time sunale

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n);
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	for (; n > 0; i++)
+	while (i < n)
 	{
-		s[i] = b;
-		n--;
+		*(s + i) = b;
+		i++;
 	}
 	return (s);
 }
