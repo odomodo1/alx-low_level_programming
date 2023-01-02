9505306a9f4c1be1c8a3887629db63320ac66600
odomodo1 <odomodo19@gmail.com>
2023-01-02 12:57:41 -0800

    ideas to build the world

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chess board
+ * @a: two dimensional array
+ */
+void print_chessboard(char (*a)[8])
+{
+	int p, k;
+
+		for (p = 0; p < 8; p++)
+		{
+			for (k = 0; k < 8; k++)
+			{
+				_putchar(a[p][k]);
+			}
+				_putchar('\n');
+		}
+}
