8295167c9e28fdada53d2e27afe60a1cd2b3c1e8
odomodo1 <odomodo19@gmail.com>
2023-01-06 15:08:27 -0800

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int k;
+
+	for (k = 0; k < argc; k++)
+	{
+		printf("%s\n", argv[k]);
+	}
+	return (0);
+}
