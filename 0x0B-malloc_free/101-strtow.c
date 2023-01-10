5deb4ce2e73f2fe7bf3f00ddb17f80298eab3dd5
odomodo1 <odomodo19@gmail.com>
2023-01-10 01:49:06 -0800

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -28,3 +28,49 @@ int count_word(char *s)
 
 	return (m);
 }
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **matrix, *tmp;
+	int n, k = 0, len = 0, words, p = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	for (n = 0; n <= len; n++)
+	{
+		if (str[n] == ' ' || str[n] == '\0')
+		{
+			if (p)
+			{
+				end = n;
+				tmp = (char *) malloc(sizeof(char) * (p + 1));
+				if (tmp == NULL)
+					return (NULL);
+
+				while (start < end)
+				*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - p;
+				k++;
+				p = 0;
+			}
+		}
+		else if (p++ == 0)
+			start = n;
+	}
+
+	matrix[k] = NULL;
+
+	return (matrix);
+}
