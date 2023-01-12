664705c7adbc687b3d1a301b337916be46c5f3bc
odomodo1 <odomodo19@gmail.com>
2023-01-12 09:14:21 -0800

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -55,39 +55,39 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int l1, l2, len, k, move, d1, d2, *result, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	len = l1 + l2 + 1;
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (k = 0; k <= l1 + l2; k++)
+	for (i = 0; i <= len1 + len2; i++)
 		result[k] = 0;
-	for (l1 = l1 - 1; l1 >= 0; l1--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		d1 = s1[l1] - '0';
-		move = 0;
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			d2 = s2[l2] - '0';
-			move += result[l1 + l2 + 1] + (d1 * d2);
-			result[l1 + l2 + 1] = move % 10;
-			move /= 10;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (move > 0)
-			result[l1 + l2 + 1] += move;
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	for (k = 0; k < len - 1; k++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (result[k])
+		if (result[i])
 			a = 1;
 		if (a)
-			_putchar(result[k] + '0');
+			_putchar(result[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
