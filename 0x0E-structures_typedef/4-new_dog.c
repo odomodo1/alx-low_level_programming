56a6a7e3b8777ed2a31111b4144bcbc8cfae3bf1
odomodo1 <odomodo19@gmail.com>
2023-01-16 10:45:34 -0800

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,31 +3,35 @@
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
+ *
  * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int p;
+	int i;
 
-	p = 0;
+	i = 0;
 
-	while (s[p] != '\0')
+	while (s[i] != '\0')
 	{
-		p++;
+		i++;
 	}
-	return (p);
+
+	return (i);
 }
+
 /**
  * _strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
+ *
  * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len, p;
+	int len, i;
 
 	len = 0;
 
@@ -35,35 +39,37 @@ char *_strcpy(char *dest, char *src)
 	{
 		len++;
 	}
-	for (p = 0; p < len; p++)
+
+	for (i = 0; i < len; i++)
 	{
-		dest[p] = src[p];
+		dest[i] = src[i];
 	}
-	dest[p] = '\0';
+	dest[i] = '\0';
+
 	return (dest);
 }
+
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
  * Return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-
 	int len1, len2;
 
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-	dog = malloc(sizeof(dog_t));
 
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 	dog->name = malloc(sizeof(char) * (len1 + 1));
-
-	if (dog->owner == NULL)
+	if (dog->name == NULL)
 	{
 		free(dog);
 		free(dog->name);
