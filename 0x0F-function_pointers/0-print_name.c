67d9a2853e49533f3fb063097d61c9b7f5e33da0
odomodo1 <odomodo19@gmail.com>
2023-01-18 00:29:00 -0800

    what is my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,8 +4,7 @@
 /**
  * print_name - print name using pointer to function
  * @name: string to add
- * @f: pointer to function
- *
+ * @f: pointer to function.
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
